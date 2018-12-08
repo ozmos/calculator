fbc1763d303b9408f72c028707c51eff1dec858c
ozmos <osamumorozumi@gmail.com>
2018-12-08 16:44:03 +0800

    Updates

---
+++ precache-manifest.31dc73308d1b938af8ac48243621b416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "d8d3b513db79b125be9a",
+    "url": "/calculator/static/js/main.d8d3b513.chunk.js"
+  },
+  {
+    "revision": "0b52df4f42ccec3371a0",
+    "url": "/calculator/static/js/1.0b52df4f.chunk.js"
+  },
+  {
+    "revision": "d8d3b513db79b125be9a",
+    "url": "/calculator/static/css/main.ae399dbf.chunk.css"
+  },
+  {
+    "revision": "b9016e16db999751a8b17f3a05d36303",
+    "url": "/calculator/index.html"
+  }
+];
